89d5a8be05581a03c6d975c2f3f2617fd6081192
Paweł Pastuszak <kotcrab@gmail.com>
2015-07-22 21:10:31 +0200

    Initial commit

---
+++ android/src/com/kotcrab/vis/demo/jumper/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kotcrab.vis.demo.jumper.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kotcrab.vis.demo.jumper.SuperJumper;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SuperJumper(), config);
+	}
+}
---
+++ desktop/src/com/kotcrab/vis/demo/jumper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kotcrab.vis.demo.jumper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kotcrab.vis.demo.jumper.SuperJumper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperJumper(), config);
+	}
+}
